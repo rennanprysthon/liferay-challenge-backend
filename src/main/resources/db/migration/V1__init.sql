ce563c1d769d685cf6ab9097a87d5c674eed4997
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-05-31 23:07:06 -0300

    added postgres database connection to the project and profiles

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,16 +1,16 @@
 CREATE TABLE product_order (
-    product_order_id IDENTITY,
+    product_order_id SERIAL,
     taxes DECIMAL NOT NULL DEFAULT 0.0,
     total DECIMAL NOT NULL DEFAULT 0.0,
     PRIMARY KEY (product_order_id)
 );
 CREATE TABLE product (
-    product_id IDENTITY,
+    product_id SERIAL,
     name VARCHAR(200) NOT NULL,
     type VARCHAR(2) DEFAULT 'OT',
     price DECIMAL NOT NULL DEFAULT 0.0,
     imported BOOLEAN NOT NULL DEFAULT FALSE,
-    cd_product_order INTEGER NULL,
+    cd_product_order INTEGER DEFAULT NULL,
     FOREIGN KEY (cd_product_order) REFERENCES product_order(product_order_id),
     PRIMARY KEY (product_id)
 );
